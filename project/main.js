ebee923c0ff462782d99f2642049addd5b7da016
Pedro Rossi <pgrr@cin.ufpe.br>
2017-05-20 21:26:14 -0300

    more fixes

--- project/main.js
+++ project/main.js
@@ -1,20 +1,21 @@
-trackLength = 0;
-tracks = {};
-var addTrack = function(instrument,value,position){
+var trackLength = 0;
+var tracks = {};
+var playingNow = {};
+
+var addTrack = function(instrument, value, position) {
   console.log(instrument, value, position);
-  if(tracks[instrument] == undefined)tracks[instrument] = {};
+  if (tracks[instrument] == undefined) tracks[instrument] = {};
   tracks[instrument][position] = value;
   trackLength = position + 1;
 }
-var playingNow = {};
 
 var loopey;
-function playGroup(){
+var playGroup = function() {
   i = 0;
-  loopey = setInterval(function(){
-    for(key in tracks){
-      if(tracks[key][i]!=undefined){
-        if(playingNow[key] || playingNow[key] == 0){
+  loopey = setInterval(function() {
+    for (key in tracks) {
+      if (tracks[key][i]!=undefined) {
+        if (playingNow[key] || playingNow[key] == 0) {
           instruments[key][playingNow[key]].pause();
           instruments[key][playingNow[key]].currentTime = 0;
         }
@@ -23,18 +24,18 @@ function playGroup(){
       }
     }
     i = i + 1;
-    if(i == trackLength)clearInterval(loopey);
+    if (i == trackLength) clearInterval(loopey);
   }, 1500);
 }
 
-var callB = function(instruments){
+var callB = function(instruments) {
   this.instruments=instruments;
 }
 
-var stopGroup = function(){
+var stopGroup = function() {
   clearInterval(loopey);
-  for(key in tracks){
-    for(x in instruments[key]){
+  for (key in tracks) {
+    for (x in instruments[key]) {
       instruments[key][x].pause();
       instruments[key][x].currentTime = 0;
     }
