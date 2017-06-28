7e237cd9f6d60cef70ab14e732e664608c7b17f6
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-06-27 21:56:41 -0300

    Four animated characters added

--- project/animation.js
+++ project/animation.js
@@ -1,10 +1,10 @@
 function sprite (options) {
   var that = {};
 
-  frameIndex = 0,
-  tickCount = 0,
-  ticksPerFrame = options.ticksPerFrame || 0;
-  numberOfFrames = options.numberOfFrames || 1;
+  that.frameIndex = 0;
+  that.tickCount = options.delay;
+  that.ticksPerFrame = options.ticksPerFrame || 0;
+  that.numberOfFrames = options.numberOfFrames || 1;
 
   that.context = options.context;
   that.width = options.width;
@@ -16,25 +16,25 @@ function sprite (options) {
    that.context.clearRect(0, 0, that.width, that.height);
    that.context.drawImage(
       that.image,
-      frameIndex * that.width / numberOfFrames,
+      that.frameIndex * that.width / that.numberOfFrames,
       0,
-      that.width / numberOfFrames,
+      that.width / that.numberOfFrames,
       that.height,
       0,
       0,
-      that.width / numberOfFrames,
+      that.width / that.numberOfFrames,
       that.height
     );
 };
 
   that.update = function () {
-    tickCount += 1;
-    if (tickCount > ticksPerFrame) {
-      tickCount = 0;
-      if (frameIndex < numberOfFrames - 1) {
-        frameIndex += 1;
+    that.tickCount += 1;
+    if (that.tickCount > that.ticksPerFrame) {
+      that.tickCount = 0;
+      if (that.frameIndex < that.numberOfFrames - 1) {
+        that.frameIndex += 1;
       }  else if (that.loop) {
-        frameIndex = 0;
+        that.frameIndex = 0;
       }
     }
   };
@@ -42,28 +42,47 @@ function sprite (options) {
   return that;
 }
 
-var canvas = document.getElementById("coinAnimation");
-canvas.width = 100;
-canvas.height = 100;
+var canvas = {};
 
-var coinImage = new Image();
-coinImage.src = "image/a.png";
+for(var i = 0; i < 4; i++){
+  canvas[i] = document.getElementById("canvas"+i)
+  canvas[i].width = 65;
+  canvas[i].height = 80;
+}
 
-var coin = sprite({
-    context: canvas.getContext("2d"),
-    width: 390,
-    height: 100,
-    image: coinImage,
-    numberOfFrames: 6,
-    ticksPerFrame: 4,
-    loop: true,
-});
+var sprites = {};
+
+for(var i = 0; i < 4; i++){
+  sprites[i] = new Image();
+}
+
+sprites[0].src = "image/spritesheet/male-sheet-1.png";
+sprites[1].src = "image/spritesheet/female-sheet-2.png";
+sprites[2].src = "image/spritesheet/female-sheet-1.png";
+sprites[3].src = "image/spritesheet/male-sheet-2.png";
+
+var animations = {};
+
+for(var i = 0; i < 4; i++){
+  animations[i] = sprite({
+      context: canvas[i].getContext("2d"),
+      width: 195,
+      height: 80,
+      image: sprites[i],
+      numberOfFrames: 3,
+      ticksPerFrame: 10,
+      loop: true,
+      delay: -25*i*i,
+  });
+}
 
 function gameLoop () {
   window.requestAnimationFrame(gameLoop);
 
-  coin.update();
-  coin.render();
+  for(var i = 0; i < 4; i++){
+    animations[i].update();
+    animations[i].render();
+  }
 }
 
-coinImage.addEventListener("load", gameLoop);
+sprites[0].addEventListener("load", gameLoop);
